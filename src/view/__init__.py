778abb009037efe869ad83fd6997761b608765e5
Peter Bierma <zintensitydev@gmail.com>
2025-06-22 17:14:24 -0400

    Improve the module __init__.

--- src/view/__init__.py
+++ src/view/__init__.py
@@ -1,3 +1,16 @@
+"""
+view.py - The Batteries-Detachable Web Framework.
+"""
 from view.__about__ import *
-from view.app import *
-from view.router import *
+
+from view import app as app
+from app import *
+
+from view import response as response
+from response import *
+
+from view import router as router
+from router import *
+
+from view import status_codes as status_codes
+from view import testing as testing
