35eac7029ce245c945aaed91e38586fcb96dff42
Peter Bierma <zintensitydev@gmail.com>
2025-06-23 20:28:46 -0400

    Add docstrings to the request methods.

--- src/view/request.py
+++ src/view/request.py
@@ -28,14 +28,58 @@ class Method(_UpperStrEnum):
     """
 
     GET = auto()
+    """
+    The GET method requests a representation of the specified resource.
+
+    Requests using GET should only retrieve data and should not contain
+    a request content.
+    """
+
     POST = auto()
+    """
+    The POST method submits an entity to the specified resource, often causing
+    a change in state or side effects on the server.
+    """
+
     PUT = auto()
+    """
+    The PUT method replaces all current representations of the target resource
+    with the request content.
+    """
+
     PATCH = auto()
+    """
+    The PATCH method applies partial modifications to a resource.
+    """
+
     DELETE = auto()
+    """
+    The DELETE method deletes the specified resource.
+    """
+
     CONNECT = auto()
+    """
+    The CONNECT method establishes a tunnel to the server identified by the
+    target resource.
+    """
+
     OPTIONS = auto()
+    """
+    The OPTIONS method describes the communication options for the target
+    resource.
+    """
+
     TRACE = auto()
+    """
+    The TRACE method performs a message loop-back test along the path to the
+    target resource.
+    """
+
     HEAD = auto()
+    """
+    The HEAD method asks for a response identical to a GET request, but
+    without a response body.
+    """
 
 
 @dataclass(slots=True)
