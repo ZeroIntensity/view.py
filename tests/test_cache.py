9cb00e519cc4a7110ac30d01a6ba24f5378d8343
Peter Bierma <zintensitydev@gmail.com>
2025-12-28 14:31:35 -0500

    Add some tests for caching.

---
+++ tests/test_cache.py
@@ -0,0 +1,59 @@
+import pytest
+from view.app import App
+from view.cache import InMemoryCache, in_memory_cache, minutes
+from view.response import ResponseLike
+from view.testing import AppTestClient
+from unittest.mock import patch
+import time
+
+
+@pytest.mark.asyncio
+async def test_in_memory_cache():
+    app = App()
+    called = 0
+
+    @app.get("/")
+    @in_memory_cache()
+    async def index() -> ResponseLike:
+        nonlocal called
+        called += 1
+        return "test"
+
+    client = AppTestClient(app)
+    await client.get("/")
+    assert called == 1
+    for _ in range(3):
+        await client.get("/")
+        assert called == 1
+
+    assert isinstance(index.view, InMemoryCache)
+    index.view.invalidate()
+    await client.get("/")
+    assert called == 2
+
+
+@pytest.mark.asyncio
+async def test_in_memory_cache_timeout():
+    app = App()
+
+    called = 0
+
+    @app.get("/")
+    @in_memory_cache(minutes(2))
+    async def index() -> ResponseLike:
+        nonlocal called
+        called += 1
+        return "test"
+
+    client = AppTestClient(app)
+    await client.get("/")
+    assert called == 1
+
+    for _ in range(100):
+        await client.get("/")
+
+    assert called == 1
+    now = time.time()
+    with patch("time.time", return_value=now + minutes(2)):
+        await client.get("/")
+        assert called == 2
