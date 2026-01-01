6a674dd00d6f0d0222a73284360005c6c23c83ef
Peter Bierma <zintensitydev@gmail.com>
2026-01-01 13:35:38 -0500

    Increase test coverage for multi-map.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -51,6 +51,11 @@ def test_empty_multi_map():
 
     assert called is False
 
+    for _ in multi_map:
+        called = True
+
+    assert called is False
+
 
 def test_multi_map_no_duplicates():
     data = [('a', 1), ('b', 2), ('c', 3)]
@@ -61,12 +66,21 @@ def test_multi_map_no_duplicates():
     assert multi_map.as_sequence() == data
 
     for key, value in data:
+        assert key in multi_map
         assert multi_map[key] == value
         assert multi_map.get_many(key) == [value]
         assert multi_map.get(key) == value
         assert multi_map.get_exactly_one(key) == value
         assert key in multi_map.keys()
         assert value in multi_map.values()
+
+    called = 0
+    for key in multi_map:
+        called += 1
+        assert key in ("a", "b", "c")
+
+    assert called == 3
+
 
 
 def test_multi_map_with_duplicates():
@@ -79,6 +93,8 @@ def test_multi_map_with_duplicates():
     assert multi_map["a"] == 1
     assert multi_map.get_many("a") == [1, 2, 3]
 
+    assert "a" in multi_map
+    assert "b" in multi_map
     assert list(multi_map.keys()) == ['a', 'b']
     assert list(multi_map.values()) == [1, 4]
     assert list(multi_map.items()) == [('a', 1), ('b', 4)]
@@ -90,6 +106,13 @@ def test_multi_map_with_duplicates():
 
     assert multi_map.get_exactly_one("b") == 4
 
+    called = 0
+    for key in multi_map:
+        called += 1
+        assert key in ("a", "b")
+
+    assert called == 2
+
 
 def test_multi_map_with_new_value():
     data = [('a', 1), ('b', 2), ('b', 3)]
@@ -98,11 +121,13 @@ def test_multi_map_with_new_value():
 
     new_map = multi_map.with_new_value('b', 4)
     assert len(new_map) == 2
+    assert "b" in new_map
     assert multi_map != new_map
     assert new_map.get_many("b") == [2, 3, 4]
 
     new_map = new_map.with_new_value("c", 4)
     assert len(new_map) == 3
+    assert "c" in new_map
     assert new_map != multi_map
     assert new_map["c"] == 4
     assert new_map.get_exactly_one("c") == 4
